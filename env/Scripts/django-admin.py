3a35c791d9e63a77e3989aa1238e431afbfaa971
TakamatsuNaoya <takamastsu15@icloud.com>
2020-10-04 00:59:05 +0900

    firstcommit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\takam\source\repos\Django-girls\techblog\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
